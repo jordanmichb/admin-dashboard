a4d64da6edc21e13b1d1bbfacfe05c3e89af5e13
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-04-11 11:35:41 -0700

    Update aside collapse styling
    Aside will smoothly collapse and expand when clicking dashboard button,
    and will smoothly translate off-page when screen size is too small. The
    content section also smoothly translates to fill space left from aside.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const searchbar = document.querySelector("#searchbar");
 const searchBtn = document.querySelector(".search-btn");
+const asideToggle = document.querySelector("aside h2");
+const aside = document.querySelector("aside");
+const content = document.querySelector(".content")
 
 searchBtn.addEventListener('click', () => {
-    searchbar.classList.toggle("invisible");
+    searchbar.classList.toggle("visible"); 
+    searchbar.focus();
 });
 
-// searchbar.addEventListener('click', () => {
-//     searchbar.classList.toggle("invisible");
-// });
+asideToggle.addEventListener('click', () => {
+    aside.classList.toggle("collapsed");
+})
 
+// if (window.screen.width <= 760) {
+//     const asideW = aside.offsetWidth;
+//     content.style.transform = `translateX(-${asideW})`
+// }
