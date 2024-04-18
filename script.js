28eb64a14f440bc55deb2189bcb5b1f83a7bbd70
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-04-18 13:58:20 -0700

    Update searchbar styling, add JavaScript comments
    Give searchbar left and right padding, set font family, and make border
    thinner.

--- script.js
+++ script.js
@@ -4,23 +4,29 @@ const asideToggle = document.querySelector("aside h2");
 const aside = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 
-
-
+/*
+ * When magnifying glass icon is clicked, searchbar collapses or expands.
+ * Focus is automatically activated. A timeout is needed so that focus
+ * is not stolen by the clicked element.
+ */
 searchBtn.addEventListener('click', () => {
     searchbar.classList.toggle("visible"); 
-    setTimeout(() => {searchbar.focus()}, 15);
+    setTimeout(() => {searchbar.focus()}, 50);
 });
 
+/*
+ * When "dashboard" header is clicked, aside collapses or expands
+ */
 asideToggle.addEventListener('click', () => {
     aside.classList.toggle("collapsed");
 });
 
-function asideHide() {
-    aside.classList.remove("collapsed");
-    aside.classList.remove("menu-expand");
-}
-
-let addSelfDestructingEventLisstener = (element, eventType, callback) => {
+/* 
+ * Menu is hidden at 760px and smaller. If use unhides menu then the 
+ * dashboard icon needs an event listener to be hidden again. It is 
+ * only needed once, so it self destructs after use.
+ */
+let addSelfDestructingEventListener = (element, eventType, callback) => {
     let handler = () => {
         callback();
         element.removeEventListener(eventType, handler);
@@ -30,11 +36,18 @@ let addSelfDestructingEventLisstener = (element, eventType, callback) => {
 
 menuBtn.addEventListener('click', () => {
     aside.classList.add("menu-expand");
-    addSelfDestructingEventLisstener(asideToggle, 'click', asideHide);
+    addSelfDestructingEventListener(asideToggle, 'click', () => {
+        aside.classList.remove("collapsed"); // Remove class added by first event listener
+        aside.classList.remove("menu-expand");
+    });
 });
 
+/*
+ * Menu is hidden at 760px and smaller. If user unhides menu then resizes to
+ * beyond 760, the class needs to be removed to preserve functionality and styling
+ */
 window.addEventListener('resize', () => {
-    if (window.screen.width > 760) {
+    if (window.screen.width > 760 && aside.classList.contains("menu-expand")) {
         aside.classList.remove("menu-expand");
     }
 });
